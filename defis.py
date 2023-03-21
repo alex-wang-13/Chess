c2e6d6d54f7b9c0c8f78cdd9734c519521e7f02b
Alex Wang <iamalex11303@gmail.com>
2023-03-21 12:34:15 -0400

    added Board Break

--- defis.py
+++ defis.py
@@ -8,6 +8,9 @@ Wang, Alex
 # used to clear the command window (i.e. os.system(clear_terminal))
 CLEAR_TERMINAL = 'clear'
 
+# used as a delimiter between boards printed to terminal
+BOARD_BREAK = '- - - - - - - -'
+
 # used to explain the rules of UCI chess notation
 # SOURCE: https://www.dcode.fr/uci-chess-notation
 UCI_EXPLANATION = 'Enter moves in UCI notation, which describes moves with the start and end coordinates of the piece (i.e. 4 characters: letter, digit, letter, digit.)'
