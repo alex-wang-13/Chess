72d41e4c91e4408cb93423d6eb70c2eed4f82080
Alex Wang <iamalex11303@gmail.com>
2023-04-23 16:34:14 -0400

    added engine

--- defis.py
+++ defis.py
@@ -14,3 +14,6 @@ BOARD_BREAK = '- - - - - - - -'
 # used to explain the rules of UCI chess notation
 # SOURCE: https://www.dcode.fr/uci-chess-notation
 UCI_EXPLANATION = 'Enter moves in UCI notation, which describes moves with the start and end coordinates of the piece (i.e. 4 characters: letter, digit, letter, digit.). To undo a move type "undo."'
+
+# evaluation engine
+STOCKFISH = r"C:\Users\15043\Spring2023Project\Chess-Bot\stockfish_15.1_win_x64_avx2\stockfish-windows-2022-x86-64-avx2.exe"
